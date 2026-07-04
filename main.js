9f99335feea885990df82efd57e1822c8c832f51
Erbár Patrik <Patrik@Patricks-MBP.home>
2026-07-04 15:59:55 +0200

    Fix Leaflet marker icons on production

--- main.js
+++ main.js
@@ -2,7 +2,17 @@ import './style.css';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-import './style.css'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 document.querySelector('#app').innerHTML = `
 <header class="top-header">
